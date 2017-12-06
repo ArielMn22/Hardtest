63b0b1d2a0cb38a79674cf5367bed7893d71d605
ArielMn22 <arielpaixao10@gmail.com>
2017-12-06 09:30:53 -0200

    CHECJ

--- Hardtest/program/.checando.sh
+++ Hardtest/program/.checando.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ "$(cat /tmp/mensagem)" == "feito" ]]; then
+	exit 0
+fi
+
 var2=$(lscpu | grep "Hypervisor vendor" | cut -d":" -f2 | sed 's/ //g' )
 var1="KVM"
 
@@ -9,9 +13,24 @@ verificacao=$?
 
 if [[ $verificacao == 0 ]] ; then
 
-  dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso esses testes não funcionarão.\nMemomria:\n-Checklist de memória\n-Teste de velocidade\n-Teste de memória\nHD\n-Teste de stress\nCPU\n-Temperatura.\n\nAgradecemos a compreensão." 80 80
+  dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso esses testes não funcionarão:\n\nMemória:\n-Checklist de memória\n-Teste de velocidade\n-Teste de memória\n\nHD:\n-Teste de stress\n\nCPU:\n-Temperatura.\n\nAgradecemos a compreensão." 80 80
 
-else
-  echo "continuando"
+echo "feito" > /tmp/mensagem
+
 fi
-echo $verificacao
+
+var3=$(lscpu | grep "Fabricante do hipervisor" | cut -d":" -f2 | sed 's/ //g' )
+var4="KVM"
+
+[[ $var3 == $var4 ]]
+
+verificacao=$?
+
+if [[ $verificacao == 0 ]] ; then
+
+  dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso esses testes não funcionarão:\n\nMemória:\n-Checklist de memória\n-Teste de velocidade\n-Teste de memória\n\nHD:\n-Teste de stress\n\nCPU:\n-Temperatura.\n\nAgradecemos a compreensão." 80 80
+
+echo "feito" > /tmp/mensagem
+
+fi
+
