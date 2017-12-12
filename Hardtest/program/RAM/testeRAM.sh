fb7b5755fe0690c2d825726bc501a206a57ec248
ArielMn22 <arielpaixao10@gmail.com>
2017-12-11 22:31:34 -0200

    Seg Dez 11 22:31:34 -02 2017

--- Hardtest/program/RAM/testeRAM.sh
+++ Hardtest/program/RAM/testeRAM.sh
@@ -109,20 +109,44 @@ MEMP(){
 		aguardando "Teste iniciado isso pode demorar um pouco..."
 	fi
 
-	memtester $TANTOMEMO $TANTOVEZES > $DIRET/RAM/resposta.txt  
-	
-	$(cat $DIRET/RAM/resposta.txt | sed 's/Random Value       /Valor Randomico      /g' > $DIRET/RAM/resposta1.txt)
-	$(cat $DIRET/RAM/resposta1.txt | sed 's/Solid Bits/Bits Solidos/g' > $DIRET/RAM/resposta2.txt)
-	$(cat $DIRET/RAM/resposta2.txt | sed 's/Sequential Increment/Incremento sequencial /g' > $DIRET/RAM/resposta3.txt)
-	$(cat $DIRET/RAM/resposta3.txt | sed 's/Block Sequential/Sequencia de Bloco/g' > $DIRET/RAM/resposta4.txt)
-	
-	ESTADO=$(echo $?)
-	
-	if [[ $ESTADO == "0" ]] ; then 
-		msg "Modulo de memória OK" 
-	else 
-		msg "Modulo de memoria com erro"
-	fi
+	memtester $TANTOMEMO $TANTOVEZES > resposta.txt  
+	ESTADO=$?
+$(cat resposta.txt | sed 's/Stuck Address   /Endereço Escolhido/g' > resposta1.txt)    
+$(cat resposta1.txt | sed 's/Random Value   /Valor Randomico  /g' > resposta2.txt)
+$(cat resposta2.txt | sed 's/Compare XOR/Compare XOR  /g' > resposta3.txt)  
+$(cat resposta3.txt | sed 's/Compare SUB/Compare SUB  /g' > resposta4.txt)
+$(cat resposta4.txt | sed 's/Compare MUL/Compare MUL  /g' > resposta5.txt)
+$(cat resposta5.txt | sed 's/Compare DIV/Comapre DIV  /g' > resposta6.txt)
+$(cat resposta6.txt | sed 's/Compare OR/Compare OR  /g' > resposta7.txt)
+$(cat resposta7.txt | sed 's/Compare AND/Compare AND  /g' > resposta8.txt)
+$(cat resposta8.txt | sed 's/Sequential Increment/Incremento sequencial /g' > resposta9.txt)
+$(cat resposta9.txt | sed 's/Solid Bits/Bits Solidos/g' > resposta10.txt)
+$(cat resposta10.txt | sed 's/Block Sequential/Sequencia de Bloco/g' > resposta11.txt)
+$(cat resposta11.txt | sed 's/Checkerboard/Checkerboard  /g' > resposta12.txt)
+$(cat resposta12.txt | sed 's/Bit Spread      /Propagação De Bits/g' > resposta13.txt)
+$(cat resposta13.txt | sed 's/Bit Flip  /Giro de Bits/g' > resposta14.txt)
+$(cat resposta14.txt | sed 's/Walking Ones      /Comportamento de Uns/g' > resposta15.txt)
+$(cat resposta15.txt | sed 's/Walking Zeroes      /Procedimento de Zeros /g' > resposta16.txt)
+$(cat resposta16.txt | sed 's/8-bit Writes    /Escrita Em 8 Bits /g' > resposta17.txt)
+$(cat resposta17.txt | sed 's/16-bit Writes   /Escrita em 16 Bits/g' > resposta18.txt)
+$(cat resposta18.txt > /tmp/respostaP.txt)
+$(rm resposta1.txt) ; $(rm resposta2.txt) ; $(rm resposta3.txt) ; $(rm resposta4.txt)
+$(rm resposta5.txt) ; $(rm resposta6.txt) ; $(rm resposta7.txt) ; $(rm resposta8.txt) 
+$(rm resposta9.txt) ; $(rm resposta10.txt) ; $(rm resposta11.txt) ; $(rm resposta12.txt)
+$(rm resposta13.txt) ; $(rm resposta14.txt) ; $(rm resposta15.txt) ; $(rm resposta16.txt) 
+$(rm resposta17.txt) ; $(rm resposta18.txt) ; $(rm resposta.txt) 
+dialog \
+--title 'Final' \
+--textbox /tmp/respostaP.txt \
+0 0
+
+	case $ESTADO in
+		0) msg "Modulo de memória OK" ;;
+		x01) msg "Erro durante alocação ou fechamento da memória." ;;
+		x02) msg "Erro durante o teste: Stuck Address" ;;
+		x04) msg "Erro durante teste inespecífico" ;;
+		*) msg "Modulo de memoria com erro desconhecido." ;;
+	esac
 
 	echo "[$(date)] Menu RAM teste de memória finalizado" >> "$LOG/hardtest$USERU.log"
 
