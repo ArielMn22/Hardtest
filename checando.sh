768c4750d52dd8d6a8a946b78ff119d5a851f245
ArielMn22 <arielpaixao10@gmail.com>
2017-12-14 16:36:11 -0200

    opa

--- checando.sh
+++ checando.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-var2=$(lscpu | grep "Hypervisor vendor" | cut -d":" -f2 & )
+var2=$(lscpu | grep "Hypervisor vendor" | cut -d":" -f2  )
+var1="KVM"
 
-
-#[[ $var2 == $var1 ]]
+[[ $var2 == $var1 ]]
 
 verificacao=$?
 
@@ -11,8 +11,6 @@ if [[ $verificacao == 0 ]] ; then
 
   dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso alguns testes não funcionarão.\n\nAgradecemos a compreensão." 80 80
   exit 0
-
+else
+  echo "continuando"
 fi
-echo $verificacao
-echo $var1
-echo $var2
