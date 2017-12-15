dfe2c3ff939c8a2b7feb63b461c6d856299a62e8
south3452 <lucas209.coutinho@gmail.com>
2017-12-15 10:05:24 -0200

    Texto

--- Hardtest/program/.checando.sh
+++ Hardtest/program/.checando.sh
@@ -9,7 +9,7 @@ verificacao=$?
 
 if [[ $verificacao == 0 ]] ; then
 
-  dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso esse teste não funcionará.\n\nAgradecemos a compreensão." 80 80
+  dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso esses testes não funcionarão.\nMemomria:\n-Checklist de memória\n-Teste de velocidade\n-Teste de memória\nHD\n-Teste de stress\nCPU\n-Temperatura.\n\nAgradecemos a compreensão." 80 80
 
 else
   echo "continuando"
