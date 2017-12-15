9fcaa4b1a5af4046b04d3c5e4ce54734ef7a2316
ArielMn22 <arielpaixao10@gmail.com>
2017-12-15 12:41:25 -0200

    check

--- Hardtest/program/.checando.sh
+++ Hardtest/program/.checando.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [[ "$(cat /tmp/mensagem)" == "feito" ]]; then
-	exit 0
-fi
 
 var2=$(lscpu | grep "Hypervisor vendor" | cut -d":" -f2 | sed 's/ //g' )
 var1="KVM"
@@ -15,7 +12,6 @@ if [[ $verificacao == 0 ]] ; then
 
   dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso esses testes não funcionarão:\n\nMemória:\n-Checklist de memória\n-Teste de velocidade\n-Teste de memória\n\nHD:\n-Teste de stress\n\nCPU:\n-Temperatura.\n\nAgradecemos a compreensão." 40 80
 
-echo "feito" > /tmp/mensagem
 
 fi
 
@@ -30,7 +26,6 @@ if [[ $verificacao == 0 ]] ; then
 
   dialog --stdout --title "OBS" --msgbox "Verificamos que você está executando nosso software em um ambiente virtual.\nPor conta disso esses testes não funcionarão:\n\nMemória:\n-Checklist de memória\n-Teste de velocidade\n-Teste de memória\n\nHD:\n-Teste de stress\n\nCPU:\n-Temperatura.\n\nAgradecemos a compreensão." 40 80
 
-echo "feito" > /tmp/mensagem
 
 fi
 
